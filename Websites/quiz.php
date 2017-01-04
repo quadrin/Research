a0dc54d262914eb98906bddeeb71081441923bc3
Ikeyskywalker <ikey@croog.com>
2017-01-03 20:19:08 -0500

    Update quiz.php

--- Websites/quiz.php
+++ Websites/quiz.php
@@ -5,19 +5,36 @@
   <link rel="stylesheet" type="text/css" href="website.css">
 </head>
 <body>
-	<form method="post" action="<">
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 		What continent are you from? <br>
-		<input type="radio" value="Asia">Asia<br>
-		<input type="radio" value="Africa">Africa<br>
-		<input type="radio" value="Australia">Australia<br>
-		<input type="radio" value="Europe">Europe<br>
-		<input type="radio" value="North America">North America<br>
-		<input type="radio" value="South America">South America<br>
-		<input type="radio" value="Antarctica">Antarctica<br>
+		<input type="radio" name="quiz" value="Asia">Asia<br>
+		<input type="radio" name="quiz" value="Africa">Africa<br>
+		<input type="radio" name="quiz" value="Australia">Australia<br>
+		<input type="radio" name="quiz" value="Europe">Europe<br>
+		<input type="radio" name="quiz" value="North America">North America<br>
+		<input type="radio" name="quiz" value="South America">South America<br>
+		<input type="radio" name="quiz" value="Antarctica">Antarctica<br>
 		<input type="submit">
 	</form>
 <?php
-	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $answer = $_POST["quiz"];
+    if ($answer == ) {
+      
+    } elseif() {
+      
+    } elseif() {
+      
+    } elseif() {
+      
+    } elseif() {
+      
+    } elseif() {
+      
+    } else {
+      
+    }
+  }
 ?>
 </body>
 </html>
