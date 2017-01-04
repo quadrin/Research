9c0668d3fd0f88ef192333915002f9fcc5363c5b
Ikeyskywalker <ikey@croog.com>
2017-01-03 20:49:26 -0500

    Update and rename quiz.php to MapPicker.php

---
+++ Websites/MapPicker.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Map Picker</title>
+  <link rel="stylesheet" type="text/css" href="website.css">
+</head>
+<body>
+	<h1>Map Picker</h1>
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		Pick a continent: <br>
+		<input type="radio" name="map" value="Asia">Asia<br>
+		<input type="radio" name="map" value="Africa">Africa<br>
+		<input type="radio" name="map" value="Australia">Australia<br>
+		<input type="radio" name="map" value="Europe">Europe<br>
+		<input type="radio" name="map" value="North America">North America<br>
+		<input type="radio" name="map" value="South America">South America<br>
+		<input type="radio" name="map" value="Antarctica">Antarctica<br>
+		<input type="submit">
+	</form>
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $answer = $_POST["map"];
+    if ($answer == "Asia") {
+			echo "<img src='http://www.wineandvinesearch.com/continents/asia.png' />"
+    } elseif($answer == "Africa") {
+      echo "<img src='http://mrnussbaum.com/images/africa.gif' />"
+    } elseif($answer == "Australia") {
+      echo "<img src='https://greenwichmeantime.com/images/index.htm_txt_australia-map.gif' />"
+    } elseif($answer == "Europe") {
+      echo "<img src='http://www.philatelicdatabase.com/wp-content/uploads/2009/04/map-europe-1871-1914.jpg' />"
+    } elseif($answer == "North America") {
+      echo "<img src='http://geology.com/world/north-america-map.gif' />"
+    } elseif($answer == "South America") {
+      echo "<img src='https://s-media-cache-ak0.pinimg.com/originals/20/f7/48/20f7486b0d754e41551fa62145bc1a44.jpg' />"
+    } elseif($answer == "Antarctica") {
+      echo "<img src='http://3belowzero.com/wp-content/uploads/2012/06/antar1.jpg' />"
+    }
+  }
+?>
+</body>
+</html>
