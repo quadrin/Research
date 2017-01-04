237141660f1df53c05323ad806c83b14fce8df65
Ikeyskywalker <ikey@croog.com>
2017-01-03 19:54:15 -0500

    Update and rename Website/website.html to Websites/website.php

---
+++ Websites/website.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Quiz</title>
+  <link rel="stylesheet" type="text/css" href="website.css">
+</head>
+<body>
+	<form>
+	
+	</form>
+<?php
+$answer = $_POST["quiz"]
+if (empty($answer)){
+echo "You have not answered"
+}
+?>
+</body>
+</html>
