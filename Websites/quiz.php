008bc3e5f7e879755a2670a48818291f1398b6f7
Ikeyskywalker <ikey@croog.com>
2017-01-03 20:10:27 -0500

    Update quiz.php

--- Websites/quiz.php
+++ Websites/quiz.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" type="text/css" href="website.css">
 </head>
 <body>
-	<form>
+	<form method="post">
 		What continent are you from? <br>
 		<input type="radio" value="Asia">Asia<br>
 		<input type="radio" value="Africa">Africa<br>
@@ -14,9 +14,10 @@
 		<input type="radio" value="North America">North America<br>
 		<input type="radio" value="South America">South America<br>
 		<input type="radio" value="Antarctica">Antarctica<br>
+		<input type="submit">
 	</form>
-<?php
-
+<?p
+	
 ?>
 </body>
 </html>
