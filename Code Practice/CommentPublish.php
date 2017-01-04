50344f8b493deca5a9c2268bf611ef34e3e6d6d6
Ikeyskywalker <ikey@croog.com>
2017-01-03 19:53:18 -0500

    Update CommentPublish.php

--- Code Practice/CommentPublish.php
+++ Code Practice/CommentPublish.php
@@ -4,11 +4,20 @@
 <title></title>
 </head>
 <body>
-  <form height="200", width="250">
-    
+  <h1>Comment Something:</h1>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="text" name="comment" size="50">
+    <input type="submit">
   </form>
 <?php
-
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $comment = $_POST['comment'];
+    if (empty($comment)) {
+      echo "Error: Comment Blank";
+    } else {
+		echo $comment;
+    }
+  }
 ?>
 </body>
 </html>
